25f90d96611ec8bf0dc8ad4633c0662ab08f9ffd
Matt Thomson <red.cataclysm@gmail.com>
2016-04-07 12:12:02 -0400

    Updates post: Website Setup

---
+++ src/blog/2016/04/07/gulpfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const tap = require('gulp-tap');
+const lazypipe = require('lazypipe');
+const del = require('del');
+const marked = require('marked');
+
+const mdTask = lazypipe()
+  .pipe(tap, _tapConcat)
+  .pipe(rename, { extname: '.html' });
+
+gulp.task('default', () => {
+  // Update specific markdown file
+  gulp.watch('./src/blog/**/*.md', (e) => {
+    console.log(`Writing ${e.path}`);
+
+    gulp.src(e.path, { base: './src/blog/' })
+      .pipe(mdTask())
+      .pipe(gulp.dest('./build/'));
+  });
+
+  // Watch for any template changes
+  gulp.watch('./src/blog/templates/**/*.html', (e) => {
+    console.log('Template Updated: Building all md files.');
+    _build();
+  });
+});
+
+gulp.task('build', _build);
+gulp.task('clean', _clean);
+
+// Concat header and footers
+function _tapConcat(f, t) {
+  f.contents = Buffer.concat([
+    new Buffer(fs.readFileSync('./src/blog/templates/header.html', 'utf8')),
+    new Buffer(marked(f.contents.toString())),
+    new Buffer(fs.readFileSync('./src/blog/templates/footer.html', 'utf8')),          
+  ]);
+}
+
+function _build() {
+  return gulp.src('./src/blog/**/*.md', {base: './src/blog/'})
+  .pipe(mdTask())
+  .pipe(gulp.dest('./build/'));
+}
+
+function _clean() {
+  del(['./build/**']);
+}
